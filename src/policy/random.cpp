066ba39f5bfc35fcd8d0195e3625ab27f5319244
Darren <108966187+Darrenop1234@users.noreply.github.com>
2023-06-17 10:17:06 +0800

    test to change files

--- src/policy/random.cpp
+++ src/policy/random.cpp
@@ -6,15 +6,15 @@
 
 /**
  * @brief Randomly get a legal action
- * 
+ *
  * @param state Now state
  * @param depth You may need this for other policy
- * @return Move 
+ * @return Move
  */
 Move Random::get_move(State *state, int depth){
   if(!state->legal_actions.size())
     state->get_legal_actions();
-  
+
   auto actions = state->legal_actions;
   return actions[(rand()+depth)%actions.size()];
 }
